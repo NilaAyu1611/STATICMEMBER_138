e6326e70954ddcc72eddd4ef664e2cab01a8a185
NilaAyu1611 <nilaayucw@gmail.com>
2023-05-15 20:42:18 +0700

    tampilkan  dengan gimana cara kita manggil fungsi (lebih ringkas baris program)

--- Chainfunction_138/Chainfunction_138.cpp
+++ Chainfunction_138/Chainfunction_138.cpp
@@ -17,3 +17,10 @@ public:
 
 } bukunya;				//objectnya
 
+int main()
+{
+	//bukunya.setJudul("Matematika");
+	//cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul();		//chain function calls
+	return 0;
+}
